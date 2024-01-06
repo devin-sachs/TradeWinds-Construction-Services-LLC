ee81dd7a70b67f1d68e051a17b9ca35322d28913
devin-sachs <75508790+devin-sachs@users.noreply.github.com>
2024-01-06 13:40:20 -0800

    Moved javascript code from main file to its own gallery-script.js file.

--- js/scripts.js
+++ js/scripts.js
@@ -53,164 +53,164 @@ window.addEventListener('DOMContentLoaded', event => {
 
 });
 
-//Accordion menu for Gallery
+// //Accordion menu for Gallery
 
-var coll = document.getElementsByClassName("collapsible");
-var i;
+// var coll = document.getElementsByClassName("collapsible");
+// var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-} 
-
-
-//Try creating a function for the below code to make this more efficient in the future. 
-
-// Reading image gallery for BEFORE Rennovations and Remodels folder 
-const beforeRennoGallery = document.getElementById('before-rennovation-and-remodels-gallery');
-
-let beforeRennovationImageIndexes = [];
-let beforeRennoLowEnd = 1;
-let beforeRennoHighEnd = 31;
-for (let rennoIndex = beforeRennoLowEnd; rennoIndex <= beforeRennoHighEnd; rennoIndex++) {
-    beforeRennovationImageIndexes.push(rennoIndex);
-}
-
-beforeRennovationImageIndexes.forEach(i => {
-    var anchor = document.createElement('a');
-    //anchor.href =`assets/images/IMG_${i}.jpg`; // //' data-lightbox = "GalleryName" data-title = "ImageCaption1"';
-    anchor.setAttribute('href',`assets/images/rennovation-and-remodels/renno-before/rennovations-and-remodels-before-${i}.jpg` );
-    anchor.setAttribute('data-lightbox', 'BeforeRennovationGallery');
-    anchor.setAttribute('data-title', 'ImageCaption1')
-    var image = document.createElement('IMG');
-    image.setAttribute('src',`assets/images/rennovation-and-remodels/renno-before/rennovations-and-remodels-before-${i}.jpg`);
-        image.setAttribute('loading',`lazy`);
-    anchor.appendChild(image);
-    beforeRennoGallery.appendChild(anchor);
-})
-
-// Reading image Gallery for AFTER Renno and remodels folder
-
-const afterRennoGallery = document.getElementById('after-rennovation-and-remodels-gallery');
-
-let rennovationImageIndexes = [];
-let rennoLowEnd = 1;
-let rennoHighEnd = 52;
-for (let rennoIndex = rennoLowEnd; rennoIndex <= rennoHighEnd; rennoIndex++) {
-    rennovationImageIndexes.push(rennoIndex);
-}
-
-rennovationImageIndexes.forEach(i => {
-    var anchor = document.createElement('a');
-    //anchor.href =`assets/images/IMG_${i}.jpg`; // //' data-lightbox = "GalleryName" data-title = "ImageCaption1"';
-    anchor.setAttribute('href',`assets/images/rennovation-and-remodels/renno-after/rennovations-and-remodels-after-${i}.jpg` );
-    anchor.setAttribute('data-lightbox', 'AfterRennovationGallery');
-    anchor.setAttribute('data-title', 'ImageCaption1')
-    var image = document.createElement('IMG');
-    image.setAttribute('src',`assets/images/rennovation-and-remodels/renno-after/rennovations-and-remodels-after-${i}.jpg`);
-        image.setAttribute('loading',`lazy`);
-    anchor.appendChild(image);
-    afterRennoGallery.appendChild(anchor);
-})
-
-// Reading image gallery for Coatings before folder 
-const beforeCoatGallery = document.getElementById('before-coatings-gallery');
-
-let beforeCoatImageIndexes = [];
-let beforeCoatLowEnd = 1;
-let beforeCoatHighEnd = 21;
-for (let coatIndex = beforeCoatLowEnd; coatIndex <= beforeCoatHighEnd; coatIndex++) {
-    beforeCoatImageIndexes.push(coatIndex);
-}
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var content = this.nextElementSibling;
+//     if (content.style.display === "block") {
+//       content.style.display = "none";
+//     } else {
+//       content.style.display = "block";
+//     }
+//   });
+// } 
 
 
-beforeCoatImageIndexes.forEach(i => {
-    var anchor = document.createElement('a');
-    anchor.setAttribute('href',`assets/images/coatings/coatings-after/coatings-after-${i}.jpg` );
-    anchor.setAttribute('data-lightbox', 'CoatingGallery');
-    anchor.setAttribute('data-title', 'ImageCaption1')
-    var image = document.createElement('IMG');
-    image.setAttribute('src',`assets/images/coatings/coatings-after/coatings-after-${i}.jpg`);
-    image.setAttribute('loading',`lazy`);
-    anchor.appendChild(image);
-    beforeCoatGallery.appendChild(anchor);
-})
+// //Try creating a function for the below code to make this more efficient in the future. 
 
-// Reading image gallery for Coatings after folder 
-const afterCoatGallery = document.getElementById('after-coatings-gallery');
+// // Reading image gallery for BEFORE Rennovations and Remodels folder 
+// const beforeRennoGallery = document.getElementById('before-rennovation-and-remodels-gallery');
 
-let afterCoatImageIndexes = [];
-let afterCoatLowEnd = 1;
-let afterCoatHighEnd = 21;
-for (let coatIndex = afterCoatLowEnd; coatIndex <= afterCoatHighEnd; coatIndex++) {
-    afterCoatImageIndexes.push(coatIndex);
-}
+// let beforeRennovationImageIndexes = [];
+// let beforeRennoLowEnd = 1;
+// let beforeRennoHighEnd = 31;
+// for (let rennoIndex = beforeRennoLowEnd; rennoIndex <= beforeRennoHighEnd; rennoIndex++) {
+//     beforeRennovationImageIndexes.push(rennoIndex);
+// }
 
+// beforeRennovationImageIndexes.forEach(i => {
+//     var anchor = document.createElement('a');
+//     //anchor.href =`assets/images/IMG_${i}.jpg`; // //' data-lightbox = "GalleryName" data-title = "ImageCaption1"';
+//     anchor.setAttribute('href',`assets/images/rennovation-and-remodels/renno-before/rennovations-and-remodels-before-${i}.jpg` );
+//     anchor.setAttribute('data-lightbox', 'BeforeRennovationGallery');
+//     anchor.setAttribute('data-title', 'ImageCaption1')
+//     var image = document.createElement('IMG');
+//     image.setAttribute('src',`assets/images/rennovation-and-remodels/renno-before/rennovations-and-remodels-before-${i}.jpg`);
+//         image.setAttribute('loading',`lazy`);
+//     anchor.appendChild(image);
+//     beforeRennoGallery.appendChild(anchor);
+// })
 
-afterCoatImageIndexes.forEach(i => {
-    var anchor = document.createElement('a');
-    anchor.setAttribute('href',`assets/images/coatings/coatings-after/coatings-after-${i}.jpg` );
-    anchor.setAttribute('data-lightbox', 'CoatingGallery');
-    anchor.setAttribute('data-title', 'ImageCaption1')
-    var image = document.createElement('IMG');
-    image.setAttribute('src',`assets/images/coatings/coatings-after/coatings-after-${i}.jpg`);
-    image.setAttribute('loading',`lazy`);
-    anchor.appendChild(image);
-    afterCoatGallery.appendChild(anchor);
-})
+// // // Reading image Gallery for AFTER Renno and remodels folder
 
-// Reading image gallery for before Gutters folder 
-const beforeGutGallery = document.getElementById('before-gutters-gallery');
+// // const afterRennoGallery = document.getElementById('after-rennovation-and-remodels-gallery');
 
-let beforeGutImageIndexes = [];
-let beforeGutLowEnd = 1;
-let beforeGutHighEnd = 13;
-for (let gutIndex = beforeGutLowEnd; gutIndex <= beforeGutHighEnd; gutIndex++) {
-    beforeGutImageIndexes.push(gutIndex);
-}
+// // let rennovationImageIndexes = [];
+// // let rennoLowEnd = 1;
+// // let rennoHighEnd = 52;
+// // for (let rennoIndex = rennoLowEnd; rennoIndex <= rennoHighEnd; rennoIndex++) {
+// //     rennovationImageIndexes.push(rennoIndex);
+// // }
 
+// // rennovationImageIndexes.forEach(i => {
+// //     var anchor = document.createElement('a');
+// //     //anchor.href =`assets/images/IMG_${i}.jpg`; // //' data-lightbox = "GalleryName" data-title = "ImageCaption1"';
+// //     anchor.setAttribute('href',`assets/images/rennovation-and-remodels/renno-after/rennovations-and-remodels-after-${i}.jpg` );
+// //     anchor.setAttribute('data-lightbox', 'AfterRennovationGallery');
+// //     anchor.setAttribute('data-title', 'ImageCaption1')
+// //     var image = document.createElement('IMG');
+// //     image.setAttribute('src',`assets/images/rennovation-and-remodels/renno-after/rennovations-and-remodels-after-${i}.jpg`);
+// //         image.setAttribute('loading',`lazy`);
+// //     anchor.appendChild(image);
+// //     afterRennoGallery.appendChild(anchor);
+// // })
 
-beforeGutImageIndexes.forEach(i => {
-    var anchor = document.createElement('a');
-    anchor.setAttribute('href',`assets/images/gutters/gutters-${i}.jpg` );
-    anchor.setAttribute('data-lightbox', 'Gutters-Gallery');
-    anchor.setAttribute('data-title', 'ImageCaption1')
-    var image = document.createElement('IMG');
-    image.setAttribute('src',`assets/images/gutters/gutters-${i}.jpg`);
-    image.setAttribute('loading',`lazy`);
-    anchor.appendChild(image);
-    beforeGutGallery.appendChild(anchor);
-})
+// // // Reading image gallery for Coatings before folder 
+// // const beforeCoatGallery = document.getElementById('before-coatings-gallery');
 
-////////////////////////////////////////
-
-// Reading image gallery for after Gutters folder 
-const afterGutGallery = document.getElementById('after-gutters-gallery');
-
-let afterGutImageIndexes = [];
-let afterGutLowEnd = 1;
-let afterGutHighEnd = 13;
-for (let gutIndex = afterGutLowEnd; gutIndex <= afterGutHighEnd; gutIndex++) {
-    afterGutImageIndexes.push(gutIndex);
-}
+// // let beforeCoatImageIndexes = [];
+// // let beforeCoatLowEnd = 1;
+// // let beforeCoatHighEnd = 21;
+// // for (let coatIndex = beforeCoatLowEnd; coatIndex <= beforeCoatHighEnd; coatIndex++) {
+// //     beforeCoatImageIndexes.push(coatIndex);
+// // }
 
 
-afterGutImageIndexes.forEach(i => {
-    var anchor = document.createElement('a');
-    anchor.setAttribute('href',`assets/images/gutters/gutters-${i}.jpg` );
-    anchor.setAttribute('data-lightbox', 'Gutters-Gallery');
-    anchor.setAttribute('data-title', 'ImageCaption1')
-    var image = document.createElement('IMG');
-    image.setAttribute('src',`assets/images/gutters/gutters-${i}.jpg`);
-    image.setAttribute('loading',`lazy`);
-    anchor.appendChild(image);
-    afterGutGallery.appendChild(anchor);
-})
+// // beforeCoatImageIndexes.forEach(i => {
+// //     var anchor = document.createElement('a');
+// //     anchor.setAttribute('href',`assets/images/coatings/coatings-after/coatings-after-${i}.jpg` );
+// //     anchor.setAttribute('data-lightbox', 'CoatingGallery');
+// //     anchor.setAttribute('data-title', 'ImageCaption1')
+// //     var image = document.createElement('IMG');
+// //     image.setAttribute('src',`assets/images/coatings/coatings-after/coatings-after-${i}.jpg`);
+// //     image.setAttribute('loading',`lazy`);
+// //     anchor.appendChild(image);
+// //     beforeCoatGallery.appendChild(anchor);
+// // })
+
+// // // Reading image gallery for Coatings after folder 
+// // const afterCoatGallery = document.getElementById('after-coatings-gallery');
+
+// // let afterCoatImageIndexes = [];
+// // let afterCoatLowEnd = 1;
+// // let afterCoatHighEnd = 21;
+// // for (let coatIndex = afterCoatLowEnd; coatIndex <= afterCoatHighEnd; coatIndex++) {
+// //     afterCoatImageIndexes.push(coatIndex);
+// // }
+
+
+// // afterCoatImageIndexes.forEach(i => {
+// //     var anchor = document.createElement('a');
+// //     anchor.setAttribute('href',`assets/images/coatings/coatings-after/coatings-after-${i}.jpg` );
+// //     anchor.setAttribute('data-lightbox', 'CoatingGallery');
+// //     anchor.setAttribute('data-title', 'ImageCaption1')
+// //     var image = document.createElement('IMG');
+// //     image.setAttribute('src',`assets/images/coatings/coatings-after/coatings-after-${i}.jpg`);
+// //     image.setAttribute('loading',`lazy`);
+// //     anchor.appendChild(image);
+// //     afterCoatGallery.appendChild(anchor);
+// // })
+
+// // // Reading image gallery for before Gutters folder 
+// // const beforeGutGallery = document.getElementById('before-gutters-gallery');
+
+// // let beforeGutImageIndexes = [];
+// // let beforeGutLowEnd = 1;
+// // let beforeGutHighEnd = 13;
+// // for (let gutIndex = beforeGutLowEnd; gutIndex <= beforeGutHighEnd; gutIndex++) {
+// //     beforeGutImageIndexes.push(gutIndex);
+// // }
+
+
+// // beforeGutImageIndexes.forEach(i => {
+// //     var anchor = document.createElement('a');
+// //     anchor.setAttribute('href',`assets/images/gutters/gutters-${i}.jpg` );
+// //     anchor.setAttribute('data-lightbox', 'Gutters-Gallery');
+// //     anchor.setAttribute('data-title', 'ImageCaption1')
+// //     var image = document.createElement('IMG');
+// //     image.setAttribute('src',`assets/images/gutters/gutters-${i}.jpg`);
+// //     image.setAttribute('loading',`lazy`);
+// //     anchor.appendChild(image);
+// //     beforeGutGallery.appendChild(anchor);
+// // })
+
+// // ////////////////////////////////////////
+
+// // // Reading image gallery for after Gutters folder 
+// // const afterGutGallery = document.getElementById('after-gutters-gallery');
+
+// // let afterGutImageIndexes = [];
+// // let afterGutLowEnd = 1;
+// // let afterGutHighEnd = 13;
+// // for (let gutIndex = afterGutLowEnd; gutIndex <= afterGutHighEnd; gutIndex++) {
+// //     afterGutImageIndexes.push(gutIndex);
+// // }
+
+
+// // afterGutImageIndexes.forEach(i => {
+// //     var anchor = document.createElement('a');
+// //     anchor.setAttribute('href',`assets/images/gutters/gutters-${i}.jpg` );
+// //     anchor.setAttribute('data-lightbox', 'Gutters-Gallery');
+// //     anchor.setAttribute('data-title', 'ImageCaption1')
+// //     var image = document.createElement('IMG');
+// //     image.setAttribute('src',`assets/images/gutters/gutters-${i}.jpg`);
+// //     image.setAttribute('loading',`lazy`);
+// //     anchor.appendChild(image);
+// //     afterGutGallery.appendChild(anchor);
+// // })
 
